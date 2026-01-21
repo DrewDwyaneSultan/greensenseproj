18b08746951604661d60114e6e95a104b43c3602
Drew Dwyane B. Sultan <dwyanelycann@gmail.com>
2026-01-21 14:02:09 +0800

    Add event listeners for nav link clicks

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('active');
 });
+// for the tirple dashes in header :))
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
